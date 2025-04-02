3325489cc6ea4b4ce57885475f641becd83aae88
Vlad Babaev <vladbabaev11@gmail.com>
2025-04-02 20:45:42 +0300

    feat: logging true

---
+++ init-db.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "🔍 Проверка, существует ли база данных..."
+if psql -U postgres -h 127.0.0.1 -tc "SELECT 1 FROM pg_database WHERE datname = 'chopp'" | grep -q 1; then
+  echo "✅ База уже есть"
+else
+  echo "🚀 Создаю базу chopp"
+  createdb -U postgres -h 127.0.0.1 chopp
+fi
+
+npm run migrate:prod &&
+npx sequelize-cli db:seed:all &&
+exec npm run start:prod
